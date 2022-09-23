6836690ffb1d516add69183149f2a24cd5c5a49d
lonanni <80687091+lonanni@users.noreply.github.com>
2022-09-23 11:17:12 +0100

    Add files via upload

--- WaterQualityFunction.py
+++ WaterQualityFunction.py
@@ -1,7 +1,27 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
-def loc_subset(df, location):
+def loc_subset(df, location_label):
+	"""
+	Return a subsample of the main dataset, according to the location of interest
+
+	input:
+	df (DataFrame object)
+	location_label (str): the location of interest, e.g. "langstone"
+	output:
+	df_subset (DataFrame object)
+
+	.. warning:: 
+	Performs **no** checks of the input.
+
+	"""
+    
+	locations = set(df['sample.samplingPoint.label'])
+	location_condition = [x for x in locations if location_label in x.lower()]
+	df_subset = df.loc[df['sample.samplingPoint.label'].isin(location_condition)]
+	return df_subset
+	
+def loc_subset_notation(df, location_notation):
 	"""
 	Return a subsample of the main dataset, according to the location of interest
 
@@ -16,9 +36,9 @@ def loc_subset(df, location):
 
 	"""
     
-	locations = set(df['sample.samplingPoint.label'])
-	location_condition = [x for x in locations if location in x.lower()]
-	df_subset = df.loc[df['sample.samplingPoint.label'].isin(location_condition)]
+	locations_notations = set(df['sample.samplingPoint.notation'])
+	location_condition = [x for x in locations_notations if location_notation in x.lower()]
+	df_subset = df.loc[df['sample.samplingPoint.notation'].isin(location_condition)]
 	return df_subset
 
 def plot_location_nutrientVStime(df, location, nutrient_determinand, ax=None,seasons=False):
@@ -111,7 +131,7 @@ def plot_location_nutrientVStime_customizable(df, location, nutrient_determinand
 
 
 
-def DAIN(df, location, ax=None, seasons=False, plt_kwargs={}, sct_kwargs={},plt_kwargs_s={}, sct_kwargs_s={}, plt_kwargs_w={}, sct_kwargs_w={}, ylabel=None):
+def DAIN(df, location, ax=None, seasons=False, plt_kwargs={}, sct_kwargs={},plt_kwargs_s={}, sct_kwargs_s={}, plt_kwargs_w={}, sct_kwargs_w={}, ylabel=None, plot_title=None):
 	if seasons is False:
 		df_env = loc_subset(df, location)
 		df_env["date"] = pd.to_datetime(df_env['sample.sampleDateTime']).dt.date
@@ -120,7 +140,8 @@ def DAIN(df, location, ax=None, seasons=False, plt_kwargs={}, sct_kwargs={},plt_
 			ax = plt.gca()
     
 		ax.scatter(df_env[df_env['determinand.notation'] == 111].sort_values(by="date", ascending=True)['date'], dain, zorder=2, **sct_kwargs)
-
+		ax.legend(fontsize=18)
+		ax.set_title(label=str(plot_title),fontsize=18)
 		ax.set_xlabel("time")
 		ax.set_ylabel(str(ylabel))
 		
@@ -140,7 +161,7 @@ def DAIN(df, location, ax=None, seasons=False, plt_kwargs={}, sct_kwargs={},plt_
 		ax.scatter(df_env_winter[df_env_winter['determinand.notation'] == 111].sort_values(by="date", ascending=True)['date'], dain_winter, zorder=2, **sct_kwargs_w, label="winter")
 		ax.plot(df_env_winter[df_env_winter['determinand.notation'] == 111].sort_values(by="date", ascending=True)['date'], dain_winter, zorder=1, **plt_kwargs_w)
 		ax.legend(fontsize=18)
-
+		ax.set_title(label=str(plot_title), fontsize=18)
 		ax.set_xlabel("time")
 		ax.set_ylabel(str(ylabel))
 	return(ax)
