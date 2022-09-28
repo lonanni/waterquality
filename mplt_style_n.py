987d9ff2f4d3bc8f589143889c0cb1d8c3e73369
lonanni <80687091+lonanni@users.noreply.github.com>
2022-09-28 14:06:52 +0100

    Add files via upload

---
+++ mplt_style_n.py
@@ -0,0 +1,16 @@
+style1 = {
+    'lines.linewidth': 2.5,
+    'lines.antialiased': True,
+    'font.family': 'serif',
+    'text.usetex' : True,
+    'axes.titlesize' : 24,
+    'axes.labelsize' : 20,
+    'xtick.labelsize' : 20,
+    'ytick.labelsize' : 20,
+    'xtick.major.size':   10,    # major tick size in points
+    'xtick.minor.size':    5,      # minor tick size in points
+    'ytick.major.size':   10,     # major tick size in points
+    'ytick.minor.size':    5,      # minor tick size in points
+    'xtick.direction':    "in",     # direction: {in, out, inout}
+    'ytick.direction':    "in",     # direction: {in, out, inout}
+}
